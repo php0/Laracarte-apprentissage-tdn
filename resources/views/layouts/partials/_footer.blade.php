de96be7d74f5dd66dd91f38c3c9631bfd890fdab
unknown <billel.loubechir@gmail.com>
2018-01-09 12:20:57 +0100

    essai gh for git premiere fois

---
+++ resources/views/layouts/partials/_footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="text-center">
+        <p>
+            &copy {{date('Y')}} · Laracarte by <a href="http:://twiter.com">@etsmo</a> - A Laramap clone app.
+        </p>
+        <p>   
+            <b> This app has been built for learning purposes.</b>     
+        </p>
+</footer>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.default')
+@section('content')
+    <h1 class="text-center">Map Here !!!!!! </h1>
+@endsection
