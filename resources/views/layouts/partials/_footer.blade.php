bd73201844eaf16c28b6ce771952ac582a2d20a9
unknown <billel.loubechir@gmail.com>
2018-01-10 14:35:58 +0100

    ajouter contact

--- resources/views/layouts/partials/_footer.blade.php
+++ resources/views/layouts/partials/_footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="text-center">
         <p>
-            &copy {{date('Y')}} · Laracarte by <a href="http:://twiter.com">@etsmo</a> - A Laramap clone app.
+            &copy {{date('Y')}} · {{config('app.name')}} by <a href="http:://twiter.com">@etsmo</a> - A Laramap clone app.
         </p>
         <p>   
             <b> This app has been built for learning purposes.</b>     
--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,21 +1,21 @@
 @extends('layouts/default',['title'=>'about'])
 @section('content')
 <div class="container">
-    <h2>What is Laracarte?</h2>
-    <p>Laracarte is a clone app of <a href="http:\\www.Laramap.com">Laramap.com</a></p>
+    <h2>What is {{config('app.name')}}?</h2>
+    <p>{{config('app.name')}} is a clone app of <a href="http:\\www.Laramap.com">Laramap.com</a></p>
     <div class="alert alert-warning" role="alert">
         <strong><i class="fa fa-exclamation-triangle" aria-hidden="true"></i> This app has been built by @etsmo for learning purposes.</strong>
     </div>
-   <p>Feel free to help to improve the <a href="https://github.com/php0/Laracarte-apprentissage-tdn"> source code</a></p> 
+   <p>Feel free to help to improve the <a href="https://github.com/secondechance/{{config('app.name')}}-apprentissage-tdn"> source code</a></p> 
     <hr>
     <h2>What is Laramap?</h2>
-    <p>Laramap is the website by which Laracarte was inspired :).</p>
+    <p>Laramap is the website by which {{config('app.name')}} was inspired :).</p>
     <p>More info <a href="">here</a> .</p>
     <hr>
-    <h2>Which tools and services are used in Laracarte?</h2>
+    <h2>Which tools and services are used in {{config('app.name')}} ?</h2>
     <p>Basically it's built on Laravel & Bootstrap. But there's a bunch of services used for email and other sections.</p>
     <ul>
-        <li> Laravel </li>
+        <li>Laravel </li>
         <li>Bootstrap</li>
         <li>Amazon S3</li>
         <li>Mandrill</li>
